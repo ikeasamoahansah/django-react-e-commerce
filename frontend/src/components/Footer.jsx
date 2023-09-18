46775f5c2e742e43f9c55bdea9e0c981b11746f5
Ike <dylanraspino264@gmail.com>
2023-09-18 00:42:27 +0000

    started styles

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+import {Component} from 'react';
+
+class Footer extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <section>
+                
+            </section>
+        );
+    }
+
+}
+
+export default Footer;
+
+
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import {Component} from 'react';
+
+class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <section>
+                
+            </section>
+        );
+    }
+
+}
+
+export default Navbar;
+
+
