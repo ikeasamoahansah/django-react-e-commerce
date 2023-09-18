750f519732b0b0677c3747e51ac257a42b80823c
Ike <dylanraspino264@gmail.com>
2023-09-18 22:00:01 +0000

    finished footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -11,19 +11,19 @@ class Footer extends Component {
             <footer className="bg-blue-400 fixed bottom-0 left-0 w-full">
                 <div className="max-w-6xl mx-auto px-4">
                     <div className="flex justify-center space-x-4 py-1">
-                        <a href="">icon</a>
-                        <a href="">icon</a>
-                        <a href="">icon</a>
-                        <a href="">icon</a>
+                        <a href="" className="text-blue-300 hover:text-gray-200 transition duration-300">icon</a>
+                        <a href="" className="text-blue-300 hover:text-gray-200 transition duration-300">icon</a>
+                        <a href="" className="text-blue-300 hover:text-gray-200 transition duration-300">icon</a>
+                        <a href="" className="text-blue-300 hover:text-gray-200 transition duration-300">icon</a>
                     </div>
-                    <div className="flex justify-center space-x-6 text-gray-200 py-1">
-                        <a href="#">Home</a>
-                        <a href="#">Products</a>
-                        <a href="#">Contact us</a>
-                        <a href="#">About us</a>
+                    <div className="flex justify-center space-x-6 py-1">
+                        <a href="#" className="text-blue-300 hover:text-gray-200 transition duration-300">Home</a>
+                        <a href="#" className="text-blue-300 hover:text-gray-200 transition duration-300">Products</a>
+                        <a href="#" className="text-blue-300 hover:text-gray-200 transition duration-300">Contact us</a>
+                        <a href="#" className="text-blue-300 hover:text-gray-200 transition duration-300">About us</a>
                     </div>
                 </div>
-                <div className="bg-blue-500 flex justify-center py-1 text-gray-200  ">
+                <div className="bg-blue-500 flex justify-center py-1 text-gray-200 hover:text-gray-300">
                         <p>Copyright &copy;2022 Designed by <span>Ike Asamoah-Ansah</span></p>
                 </div>
             </footer>
