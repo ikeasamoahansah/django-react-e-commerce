17233ae8d39128a1b460b34166a11b6ac1e5cba8
Ike <dylanraspino264@gmail.com>
2023-09-18 23:23:28 +0000

    finished login

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -7,8 +7,38 @@ class Login extends Component{
 
     render(){
         return (
-            <>
-            </>
+            <main className="min-h-screen flex flex-col items-center justify-center">
+                <div className="space-y-3 my-5">
+                    <h1 className="text-3xl font-extrabold">Sign in to your account</h1>
+                    <p className="text-center">
+                        or 
+                        <a href="#" className="text-blue-400 border-b border-blue-400"> register your free account</a>
+                    </p>
+                </div>
+                <div className="max-w-md w-full mx-auto bg-gray-200 rounded-lg p-7 space-y-3">
+                    <div className="flex flex-col">
+                        <label className="mb-1 text-sm" htmlFor="email">Email</label>
+                        <input className="border rounded-md px-3 py-2" type="email" name="email" placeholder="email@example.com" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="mb-1 text-sm" htmlFor="password">Password</label>
+                        <input className="border rounded-md px-3 py-2" type="password" name="password" />
+                    </div>
+                    <div>
+                        <button className="w-full rounded-md bg-blue-400 text-white py-2">Sign in</button>
+                    </div>
+                    <div className="flex justify-between text-sm">
+                        <div className="flex items-center space-x-2">
+                            <input type="checkbox" name="rememberme" />
+                            <label htmlFor="rememberme">Remember Me</label>
+                        </div>
+                        <div>
+                            <a href="#" className="text-blue-400">Forgot Password?</a>
+                        </div>
+                    </div>
+                    <div></div>
+                </div>
+            </main>
         );
     }
 
